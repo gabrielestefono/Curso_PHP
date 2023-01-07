eaf9558c3fdacf1ecd66dd4a7ce64c9e379d53a5
gabrielestefono <gabrielestefono@hotmail.com>
2023-01-07 10:41:29 -0300

    07/01/2023 3º Commit

---
+++ Projeto06/12-Switch/index.php
@@ -0,0 +1,31 @@
+<?php
+
+// Tentar lembrar essa!!!
+
+$x = 5;
+
+switch($x){
+    case 0:
+        echo "X é igual a 0 <br>";
+        break;
+    case 1:
+        echo "X é igual a 1 <br>";
+        break;
+    case 2:
+        echo "X é igual a 2 <br>";
+        break;
+    case 3:
+        echo "X é igual a 3 <br>";
+        break;
+    case 4:
+        echo "X é igual a 4 <br>";
+        break;
+    case 5:
+        echo "X é igual a 5 <br>";
+        break;
+    case 6:
+        echo "X é igual a 6 <br>";
+        break;
+    default:
+        echo "X não é igual a nenhum dos casos <br>";
+}
