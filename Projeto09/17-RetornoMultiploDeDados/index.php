eaab6b1eef317311477ecde80264419c9a74c83a
gabrielestefono <gabrielestefono@hotmail.com>
2023-01-12 12:20:37 -0300

    12/01/2023 4º Commit

---
+++ Projeto09/17-RetornoMultiploDeDados/index.php
@@ -0,0 +1,15 @@
+<?php
+
+function alteraDados($nome, $idade){
+    $nome = "Sr. $nome";
+    $idade = "$idade anos";
+    return [$nome, $idade];
+}
+
+$dados = alteraDados("Gabriel", 26);
+
+print_r($dados);
+
+echo "<br>";
+
+echo "Olá $dados[0], você tem $dados[1]!";
