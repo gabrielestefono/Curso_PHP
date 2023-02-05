262074f211b4eb2086f10146cb44025ee2a74cbb
gabrielestefono <gabrielestefono@hotmail.com>
2023-02-05 10:18:05 -0300

    05/02/2023 - 1º Commit

---
+++ Projeto11/22-BuscandoEmArrays/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$arr = ['Banana', "Maçã", "Batata", "Pera", "Mamão"];
+
+if(in_array("Batata", $arr)){
+    echo "Sim, nesse array tem Batata";
+}else{
+    echo "Não, nesse array não tem Batata";
+}
--- Projeto11/23-OrdenacaoEmArrays/index.php
+++ Projeto11/23-OrdenacaoEmArrays/index.php
@@ -1,3 +1,14 @@
 <?php
 
+$arr = [1,2,3,4,5,6,7,8,9,10];
+$arr2 = ["Batata", "Limão", "Laranja", "Morango"];
+
+rsort($arr);
+print_r($arr);
+
+echo "<br>";
+
+sort($arr2);
+print_r($arr2);
+
 ?>
