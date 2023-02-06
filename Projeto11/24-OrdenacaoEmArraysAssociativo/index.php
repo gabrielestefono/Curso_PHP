78c5e5b0248be60e328e17ed6b1499620ddc2fa0
gabrielestefono <gabrielestefono@hotmail.com>
2023-02-06 11:49:09 -0300

    06/02/2023 - 1º Commit

--- Projeto11/24-OrdenacaoEmArraysAssociativo/index.php
+++ Projeto11/24-OrdenacaoEmArraysAssociativo/index.php
@@ -1,3 +1,19 @@
 <?php
 
+$arr = [
+    "Mateus" => 29,
+    "João" => 18,
+    "Luiz" => 25,
+    "Maria" => 12
+];
+arsort($arr);
+print_r($arr);
+
+echo "<br>";
+asort($arr);
+print_r($arr);
+
+
+
+
 ?>
--- Projeto11/25-Atividade/index.php
+++ Projeto11/25-Atividade/index.php
@@ -1,3 +1,25 @@
 <?php
 
+$arr = [
+    "Mateus" => 2918,
+    "João" => 18230,
+    "Luiz" => 25520,
+    "Maria" => 1255,
+    "Roberta" => 5500,
+    "Vinicius" => 9900,
+    "Leticia" => 153291
+];
+
+arsort($arr);
 ?>
+
+<table style="border: 1px solid black;">
+    <tr style="border: 1px solid black;">
+        <th style="border: 1px solid black;">Posição</th><th style="border: 1px solid black;">Jogador</th><th style="border: 1px solid black;">Pontuação</th>
+    </tr>
+    <?php $i = 0; foreach($arr as $nome => $valor):?>
+    <tr>
+        <td style="border: 1px solid black; text-align: center;"><?= ++$i ?></td><td style="border: 1px solid black; text-align: center;"><?=$nome?></td><td style="border: 1px solid black; text-align: center;"><?=$valor?></td>
+    </tr>
+    <?php endforeach;?>
+</table>
