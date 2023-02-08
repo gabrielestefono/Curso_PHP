0969c891aa517e74c25ca14d4eb53119c172c03d
gabrielestefono <gabrielestefono@hotmail.com>
2023-02-07 21:09:50 -0300

    07/02/2023 - 1º Commit

--- Projeto11/26-InvertendoArrays/index.php
+++ Projeto11/26-InvertendoArrays/index.php
@@ -1,3 +1,9 @@
 <?php
 
+$arr = [1,2,3,4,5];
+$rev = array_reverse($arr);
+print_r($arr);
+echo "<br>";
+print_r($rev);
+echo "<br>";
 ?>
--- Projeto11/27-ArraysAleatorios/index.php
+++ Projeto11/27-ArraysAleatorios/index.php
@@ -1,3 +1,10 @@
 <?php
 
+$arr = [1,2,3,4,5];
+print_r($arr);
+echo "<br>";
+shuffle($arr);
+print_r($arr);
+echo "<br>";
+
 ?>
