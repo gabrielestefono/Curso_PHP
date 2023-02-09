fc18d15bc4626ac7c18c1d716d6be209f8f3a446
gabrielestefono <gabrielestefono@hotmail.com>
2023-02-09 11:12:03 -0300

    09/02/2023 - 1º Commit

--- Projeto11/30-DiferencaEntreArrays/index.php
+++ Projeto11/30-DiferencaEntreArrays/index.php
@@ -1,3 +1,18 @@
 <?php
 
-?>
+$arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$arr2 = [2, 3, 44, 5, 6, 77, 8, 9, 10];
+
+
+$dif = array_diff($arr1, $arr2);
+
+print_r($dif);
+
+echo "<br>";
+
+$dif1 = array_diff($arr2, $arr1);
+
+print_r($dif1);
+
+echo "<br>";
+
