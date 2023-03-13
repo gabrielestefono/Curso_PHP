51e821e83b8b6cd90fe32b7064cdae387c35dc4e
gabrielestefono <gabrielestefono@hotmail.com>
2023-03-12 21:16:07 -0300

    12/03/2023 - 1º Commit

---
+++ Projeto16/12-conexaoPDO/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$db = "cursophp";
+$user = "gabriel";
+$password = "teste123";
+$conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+
+// ASSUNTO DA AULA
+
+?>
