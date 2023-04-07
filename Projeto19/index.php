312eaf465df4ef49f825432fa77c037a9cb2362f
gabrielestefono <gabrielestefono@hotmail.com>
2023-04-06 21:00:21 -0300

    Adicionando Bootstrap

--- Projeto19/index.php
+++ Projeto19/index.php
@@ -5,9 +5,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>MovieStar</title>
 	<link rel="short icon" href="img/moviestar.ico">
+	<!-- Adicionando Bootstrap -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.css" integrity="sha512-r0fo0kMK8myZfuKWk9b6yY8azUnHCPhgNz/uWDl2rtMdWJlk7gmd9socvGZdZzICwAkMgfTkVrplDahQ07Gi0A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<!-- Adicionando CSS -->
 	<link rel="stylesheet" href="css/styles.css">
+	<!-- Adicionando JavaScript -->
 </head>
 <body>
 	<h1>Testando Movie Star</h1>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.js" integrity="sha512-ipBeSMCnlAvS4AEbycy0nTk9KkYr5lUJwFHNvf4IxtV/CDW4qx53mZKUryWtNr6GFaBl11EXyrU6iE3mo6ib2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
