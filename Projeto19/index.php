c4394bdbbbd3d6a0f1f0b2c9a5f21816b0ef5f9c
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-02 11:13:51 -0300

    Criando conexão com o banco de dados

--- Projeto19/index.php
+++ Projeto19/index.php
@@ -1,28 +1,33 @@
+<?php
+  require_once "globals.php";
+  require_once "db.php";
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>MovieStar</title>
-	<link rel="short icon" href="img/moviestar.ico">
-	<!-- Adicionando Bootstrap -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.css"
-	integrity="sha512-r0fo0kMK8myZfuKWk9b6yY8azUnHCPhgNz/uWDl2rtMdWJlk7gmd9socvGZdZzICwAkMgfTkVrplDahQ07Gi0A=="
-	crossorigin="anonymous" referrerpolicy="no-referrer" />
-	<!-- Adicionando CSS -->
-	<link rel="stylesheet" href="css/styles.css">
-	<!-- FontAwesome -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-	integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-	crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MovieStar</title>
+  <link rel="short icon" href="img/moviestar.ico">
+  <!-- Adicionando Bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.css"
+  integrity="sha512-r0fo0kMK8myZfuKWk9b6yY8azUnHCPhgNz/uWDl2rtMdWJlk7gmd9socvGZdZzICwAkMgfTkVrplDahQ07Gi0A=="
+  crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- Adicionando CSS -->
+  <link rel="stylesheet" href="css/styles.css">
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+  integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+  crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-	<h1>Testando Movie Star</h1>
-	<i class="far fa-plus-square"></i>
-	<!-- Bootstrap Javascript -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.js"
-	integrity="sha512-ipBeSMCnlAvS4AEbycy0nTk9KkYr5lUJwFHNvf4IxtV/CDW4qx53mZKUryWtNr6GFaBl11EXyrU6iE3mo6ib2g=="
-	crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <h1>Testando Movie Star</h1>
+  <i class="far fa-plus-square"></i>
+  <!-- Bootstrap Javascript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.js"
+  integrity="sha512-ipBeSMCnlAvS4AEbycy0nTk9KkYr5lUJwFHNvf4IxtV/CDW4qx53mZKUryWtNr6GFaBl11EXyrU6iE3mo6ib2g=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
